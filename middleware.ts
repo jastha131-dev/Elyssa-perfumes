b1dfc81f3b16b198cf501b789032d8480bd83cda
california-media <web@californiamediauae.com>
2026-05-15 17:39:02 +0400

    feat: add next-intl locale routing middleware

---
+++ middleware.ts
@@ -0,0 +1,13 @@
+import createMiddleware from 'next-intl/middleware'
+
+export default createMiddleware({
+  locales: ['en', 'ar'],
+  defaultLocale: 'en',
+  localePrefix: 'always'
+})
+
+export const config = {
+  matcher: [
+    '/((?!api|studio|_next|_vercel|favicon\\.ico|.*\\..*).*)'
+  ]
+}
